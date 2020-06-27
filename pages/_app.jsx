d2bf92546b32794eca5119665fcd68499b60ab65
Anthony Li <li.anthony917@gmail.com>
2020-06-27 18:21:47 -0400

    Add site stuff

---
+++ pages/_app.jsx
@@ -0,0 +1,5 @@
+import 'scss/main.scss';
+
+export default function App({Component, pageProps}) {
+    return <Component {...pageProps} />;
+}
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Page from 'components/Page';
+
+export default function Home() {
+    return <Page>
+        <h1>Hello, world!</h1>
+    </Page>;
+}
