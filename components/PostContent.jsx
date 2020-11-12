21000e6e588fcbed07eb86fdd0501a7a84998c07
Anthony Li <me@anli.dev>
2020-11-12 00:17:24 -0500

    Import Router

--- components/PostContent.jsx
+++ components/PostContent.jsx
@@ -1,3 +1,5 @@
+import Router from 'next/router';
+
 export default function PostContent({html}) {
    return <div className="page-content" dangerouslySetInnerHTML={{__html: html}} onClick={(e) => {
         const link = e.target.closest(".page-content a");
