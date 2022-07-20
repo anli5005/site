9f20a0548d0f7216651c6d8a25f935c838631338
Anthony Li <me@anli.dev>
2022-07-20 17:35:01 -0400

    Start over

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,55 +3,10 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 
 import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false;
-
-import { ThemeProvider, createGlobalStyle } from 'styled-components';
-import Router from 'next/router';
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
-import theme from '../theme';
 import { DefaultSeo } from 'next-seo';
 
-Router.events.on("routeChangeStart", (_, {shallow}) => {
-    if (!shallow) NProgress.start();
-});
-["routeChangeComplete", "routeChangeError"].forEach(event => Router.events.on(event, (_, {shallow}) => {
-    if (!shallow) NProgress.done();
-}));
-
-const GlobalStyle = createGlobalStyle`
-:root {
-${({theme: {colors: {light, names}}}) => names.map(([key, value]) => {
-    return `--${value}: ${light[key]};\n`;
-}).join("\n")}}
-}
-
-@media (prefers-color-scheme: dark) {
-:root {
-${({theme: {colors: {dark, names}}}) => names.map(([key, value]) => {
-    return `--${value}: ${dark[key]};\n`;
-}).join("\n")}
-}
-
-.dark-invert {
-    filter: invert(1) hue-rotate(180deg);
-}
-}
-
-.white-box {
-    border-radius: 0.25rem;
-    background-color: white;
-    width: auto;
-    padding: ${props => props.theme.spacing.sm};
-    display: inline-block;
-}
-
-.syntax-highlighted-code {
-    border-radius: 0.25rem;
-}
-`;
-
 export default function App({Component, pageProps}) {
-    return <ThemeProvider theme={theme}>
+    return <>
         <DefaultSeo
             openGraph={{
                 type: "website",
@@ -70,7 +25,6 @@ export default function App({Component, pageProps}) {
                 cardType: "summary"
             }}
         />
-        <GlobalStyle />
         <Component {...pageProps} />
-    </ThemeProvider>;
+    </>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -28,44 +28,6 @@ module.exports = {
                     "800": "#7b10f3",
                 }
             },
-            typography: ({theme}) => ({
-                ocean: {
-                    css: {
-                        "--tw-prose-body": theme("colors.ocean[950]"),
-                        "--tw-prose-headings": theme("colors.ocean[950]"),
-                        "--tw-prose-lead": theme("colors.ocean[950]"),
-                        "--tw-prose-links": theme("colors.ocean[500]"),
-                        "--tw-prose-bold": theme("colors.ocean[950]"),
-                        "--tw-prose-counters": theme("colors.ocean[950]"),
-                        "--tw-prose-bullets": theme("colors.ocean[900]"),
-                        "--tw-prose-hr": theme("colors.ocean[100]"),
-                        "--tw-prose-quotes": theme("colors.ocean[950]"),
-                        "--tw-prose-quote-borders": theme("colors.ocean[100]"),
-                        "--tw-prose-captions": theme("colors.ocean[950]"),
-                        "--tw-prose-code": theme("colors.ocean[950]"),
-                        "--tw-prose-pre-code": theme("colors.ocean[950]"),
-                        "--tw-prose-pre-bg": theme("colors.ocean[950]"),
-                        "--tw-prose-th-borders": theme("colors.ocean[100]"),
-                        "--tw-prose-td-borders": theme("colors.ocean[100]"),
-                        "--tw-prose-invert-body": theme("colors.ocean[100]"),
-                        "--tw-prose-invert-headings": theme("colors.ocean[100]"),
-                        "--tw-prose-invert-lead": theme("colors.ocean[100]"),
-                        "--tw-prose-invert-links": theme("colors.ocean[300]"),
-                        "--tw-prose-invert-bold": theme("colors.ocean[100]"),
-                        "--tw-prose-invert-counters": theme("colors.ocean[100]"),
-                        "--tw-prose-invert-bullets": theme("colors.ocean[200]"),
-                        "--tw-prose-invert-hr": theme("colors.gray[600]"),
-                        "--tw-prose-invert-quotes": theme("colors.ocean[100]"),
-                        "--tw-prose-invert-quote-borders": theme("colors.gray[600]"),
-                        "--tw-prose-invert-captions": theme("colors.ocean[100]"),
-                        "--tw-prose-invert-code": theme("colors.ocean[100]"),
-                        "--tw-prose-invert-pre-code": theme("colors.ocean[100]"),
-                        "--tw-prose-invert-pre-bg": theme("colors.ocean[100]"),
-                        "--tw-prose-invert-th-borders": theme("colors.gray[600]"),
-                        "--tw-prose-invert-td-borders": theme("colors.gray[600]"),
-                    }
-                }
-            }),
         },
         fontFamily: {
             sans: ["Europa", "europa", "ui-sans-serif", '"Helvetica Neue"', "Helvetica", "arial", "sans-serif"],
@@ -73,7 +35,4 @@ module.exports = {
             mono: ['"Fira Code"', '"Fira Mono"', "ui-monospace", "Menlo", "Monaco", "Consolas", '"Liberation Mono"', '"Courier New"', "monospace"],
         },
     },
-    plugins: [
-        require("@tailwindcss/typography"),
-    ],
-}
+};
